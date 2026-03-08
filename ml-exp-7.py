176d1a69466b5401980c68430808e0a83f555208
Sachith Kasa <kasasachith@gmail.com>
2026-03-08 21:23:34 +0530

    Add SVM classification for Iris dataset

---
+++ ml-exp-7.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+from sklearn import svm #Used for classification and regression
+
+iris = pd.read_csv(r"C:\Users\SACHITH\Downloads\Iris - Iris.csv")
+iris.head()
+
+X = iris.iloc[:, :-1]
+y = iris.iloc[:, -1]
+
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+
+X =scaler.fit_transform(X)
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+
+clf = svm.SVC(kernel='rbf')
+clf.fit(X_train, y_train)
+
+y_pred = clf.predict(X_test)
+
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(y_test, y_pred)
+print(accuracy)
